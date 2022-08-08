64132f060b1a9c7978d403063bb6f251808723eb
ChrisCherng <christopher.cherng@gmail.com>
2022-08-08 19:21:40 +0000

    add initial views and basic navbar, add materialize

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+# from .models import Room
 
-# Create your views here.
+def RoomList(request):
+    # model = Room
+    # queryset = Room.objects.filter(status=1)
+    return render(request,'index.html')
