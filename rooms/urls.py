aec43ec9503eb5913d740fd289f368f14c41fae0
ChrisCherng <christopher.cherng@gmail.com>
2022-08-21 19:21:04 +0000

    add templates and routing for room details

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RoomList.as_view(), name='home'),
+    path('<slug:slug>/', views.RoomDetail.as_view(), name='room_detail'),
     path('contact', views.ContactForm, name='contact_form'),
     path('booking', views.BookingPage, name='booking_page')
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Room
+
 
 class RoomList(generic.ListView):
     model = Room
     queryset = Room.objects.filter(status=1)
     template_name = 'index.html'
+
+
+class RoomDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Room.objects.filter(status=1)
+        room = get_object_or_404(queryset, slug=slug)
+        return render(
+            request, 'room_detail.html', {
+                "room": room
+            },
+        )
+
 
 def ContactForm(request):
     return render(request,'contact.html')
